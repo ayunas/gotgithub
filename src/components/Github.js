900283dbf46fba12d283626a55ab138c8767dc20
ayunas <ayunas@gmail.com>
2019-04-13 18:02:04 -0500

    styled elements on form page using flexbox

--- src/components/Github.js
+++ src/components/Github.js
@@ -9,7 +9,7 @@ const Github = props => {
             <Link className='Linky'to='/'><button>Find Another Github User</button></Link>
             {props.e ? <h1>Github User: {props.name}</h1> : <h3>No User Found</h3>}
             { props.e && <img src={props.avatar}/> }
-            { props.e && <p>Username: {props.name ? props.name :<span>N/A</span>} </p> }
+            { props.e && <p><strong>Username:</strong> {props.name ? props.name :<span>N/A</span>} </p> }
             { props.e && <p>About: {props.bio ? props.bio : <span>N/A</span>} </p> }
             { props.e && <p>Location: {props.location ? props.location : <span>N/A</span>} </p> }
             { props.e && <p>Number of Repos: {props.repos ? props.repos : <span>N/A</span>} </p> }
