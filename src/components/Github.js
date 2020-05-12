d343845c85e8896687197aca63461bf08837fccc
Caleb Waters <cw@mekanation.com>
2020-05-12 11:08:59 -0500

    Fixed github user displaying with Name instead of Username.

--- src/components/Github.js
+++ src/components/Github.js
@@ -7,7 +7,7 @@ const Github = props => {
     return (
         <div className='Github'>
             <Link className='Linky'to='/'><button>Find Another Github User</button></Link>
-            {props.e ? <h1>Github User: {props.name}</h1> : <h3>No User Found</h3>}
+            {props.e ? <h1>Github User: {props.login}</h1> : <h3>No User Found</h3>}
             { props.e && <img src={props.avatar}/> }
             { props.e && <p><strong>Username:</strong> {props.name ? props.name :<span>N/A</span>} </p> }
             { props.e && <p>About: {props.bio ? props.bio : <span>N/A</span>} </p> }
